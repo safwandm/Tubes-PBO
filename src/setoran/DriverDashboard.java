ef3bfd300e1e4b009e5426195e9b3a454666fd6d
safwandm <safwan.damanik@gmail.com>
2024-04-23 22:04:59 +0700

    Login success

--- src/setoran/DriverDashboard.java
+++ src/setoran/DriverDashboard.java
@@ -15,14 +15,17 @@ public class DriverDashboard {
         Dashboard d = new Dashboard();
         KatalogM k = new KatalogM();
         
-        Scanner scan = new Scanner(System.in);
+        Scanner scanInt = new Scanner(System.in);
+        Scanner scanStr = new Scanner(System.in);
         
         int input;
+        String inputStr;
+        
         boolean loop = true;
         
         while (loop) {
             System.out.print("\n1. Login\n2. Register\n0. Exit\nInput: ");
-            input = scan.nextInt();
+            input = scanInt.nextInt();
             
             if (input == 1) {
                 d.login();
@@ -31,6 +34,64 @@ public class DriverDashboard {
             } else {
                 loop = false;
             }
+            
+            while (d.currentUser != null) {
+                if (d.currentUser instanceof Pelanggan) {
+                    Pelanggan user = (Pelanggan)d.currentUser;
+                    System.out.print("\n1. Sewa motor\n2. Info\n0. Logout\nInput: ");
+                    input = scanInt.nextInt();
+                    
+                    switch (input) {
+                        case 1:
+                            System.out.println("Pilih motor yang tersedia: ");
+                            int i = 0;
+                            for (Motor m: k.motorAktif()) {
+                                System.out.println(++i +" "+ m.getNama());
+                            }
+                            System.out.print("Input: ");
+                            input = scanInt.nextInt();
+                            user.sewaM(input, k);
+                            break;
+                            
+                        case 2:
+                            d.currentUser.UserInfo();
+                            break;
+                        case 0:
+                            d.logout();
+                            continue;
+                        default:
+                            System.out.println("Input tidak dikenali");
+                    }
+
+                } else {
+                    Mitra user = (Mitra)d.currentUser;
+                    System.out.print("\n1. Menyewa\n2. Info\n0. Logout\nInput: ");
+                    input = scanInt.nextInt();
+                    
+                    switch (input) {
+                        case 1:
+                            String nama;
+                            int harga;
+                            System.out.println("\nTulis Deskripsi motor");
+                            System.out.print("Nama: ");
+                            nama = scanStr.nextLine();
+                            System.out.print("Harga: ");
+                            harga = scanInt.nextInt();
+                            user.menyewakanM(new Motor(nama, user, harga), k);
+                            break;
+                            
+                        case 2:
+                            d.currentUser.UserInfo();
+                            break;
+                        case 0:
+                            d.logout();
+                            continue;
+                        default:
+                            System.out.println("Input tidak dikenali");
+                    }
+                }
+            }
+           
             
         }
         
