8d1e09cc7e3d33a35c1db15d250da37b225a8efd
Ago-oyu <95378690+Ago-oyu@users.noreply.github.com>
2024-04-24 13:48:11 +0700

    add LoginPage & RegisterPage

---
+++ src/GUI/ModelUser.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GUI;
+
+import setoran.DatabaseUser;
+
+/**
+ *
+ * @author ASUS
+ */
+public class ModelUser {
+    public static DatabaseUser databaseUser = new DatabaseUser();
+}
