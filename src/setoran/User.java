d19babc68f56c379a3b103ee4a2018eae647d750
Yoga Fikri <95378690+Ago-oyu@users.noreply.github.com>
2024-06-01 16:45:19 +0700

    edit constructor method

--- src/setoran/User.java
+++ src/setoran/User.java
@@ -10,13 +10,31 @@ package setoran;
  * @author daman1k
  */
 public abstract class User {
-    private String username, password;
-    private int age;
+    private String username, password, tipeAkun;
+    private int idUser, umur;
 
-    public User(String username, String password, int age) {
+    public User(int idUser, String username, String password, int umur, String tipeAkun) {
+        this.idUser = idUser;
         this.username = username;
         this.password = password;
-        this.age = age;
+        this.umur = umur;
+        this.tipeAkun = tipeAkun;
+    }
+
+    public String getTipeAkun() {
+        return tipeAkun;
+    }
+
+    public void setTipeAkun(String tipeAkun) {
+        this.tipeAkun = tipeAkun;
+    }
+
+    public int getIdUser() {
+        return idUser;
+    }
+
+    public void setIdUser(int idUser) {
+        this.idUser = idUser;
     }
 
     public String getUsername() {
@@ -35,12 +53,12 @@ public abstract class User {
         this.password = password;
     }
 
-    public int getAge() {
-        return age;
+    public int getUmur() {
+        return umur;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    public void setUmur(int umur) {
+        this.umur = umur;
     }
     
     
